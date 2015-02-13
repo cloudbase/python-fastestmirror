05b0ade9dc099041ff77f4720fdd4e1dbdfc7b72
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2015-02-13 15:09:08 +0200

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='fastestmirror',
+      version='0.3.3',
+      description='Standalone version of yum-util\'s fastestmirror plugin',
+      url='https://github.com/cloudbase/python-fastestmirror',
+      py_modules=['fastestmirror'],
+     )
+	 
